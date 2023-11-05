c12814b1298f3499dd665e39c783a390343805a3
DillTheBeast <dillonmaltese@gmail.com>
2023-11-05 11:39:19 -0500

    ASD

--- hello.cpp
+++ hello.cpp
@@ -7,6 +7,11 @@ using namespace std;
 
 //Args can be: int argc, char *argv[] but don't need
 int main() {
-    //Standard output + insertion operator + 
-    cout << "Hello, world!\n";
-}
+    //Always want a \n at end of program because then new code tries to go with it
+    //cout = System.out.print
+    //cin = System.in
+    int age;
+    cout << "What is your age? ";
+    cin >> age;
+    cout << "Your age is " << age << endl;` 1
+}`
