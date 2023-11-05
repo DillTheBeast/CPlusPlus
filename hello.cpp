7a206bd7811d85f0f08be33794d1cccd75e0600d
DillTheBeast <dillonmaltese@gmail.com>
2023-11-05 11:23:32 -0500

    asd

--- hello.cpp
+++ hello.cpp
@@ -3,4 +3,10 @@
 // By Dillon Maltese
 
 #include <iostream>
+using namespace std;
 
+//Args can be: int argc, char *argv[] but don't need
+int main() {
+    //Standard output + insertion operator + 
+    cout << "Hello, world!";
+}
